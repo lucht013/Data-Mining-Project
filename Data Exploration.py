147e32629a59a9b0a0f14d28e7a58ff9a67ddb77
David <lucht013@morris.umn.edu>
2020-11-09 16:55:53 -0600

    Add Data Exploration.py for data cleaning and exploration step

---
+++ Data Exploration.py
@@ -0,0 +1,55 @@
+'''
+Column Legend
+"timestamp" - timestamp field for grouping the data
+"cnt" - the count of a new bike shares
+"t1" - real temperature in C
+"t2" - temperature in C "feels like"
+"hum" - humidity in percentage
+"windspeed" - wind speed in km/h
+"weathercode" - category of the weather
+      1 = Clear ; mostly clear but have some values with haze/fog/patches of fog/ fog in vicinity
+      2 = scattered clouds / few clouds
+      3 = Broken clouds
+      4 = Cloudy
+      7 = Rain/ light Rain shower/ Light rain
+      10 = rain with thunderstorm
+      26 = snowfall
+      94 = Freezing Fog
+"isholiday" - boolean field - 1 holiday / 0 non holiday
+"isweekend" - boolean field - 1 if the day is weekend
+"season" - category field meteorological seasons: 0-spring ; 1-summer; 2-fall; 3-winter.
+'''
+
+import pandas as pd
+import numpy as np
+
+bikes = pd.read_csv("london_merged.csv")
+
+#Looking through the data
+
+print(bikes.columns)
+
+print("Describing dataset:")
+print(bikes.describe(include='all'))
+print()
+
+print("First five of each column:")
+print(bikes.head())
+print()
+
+print("Last five of each column:")
+print(bikes.tail())
+print()
+
+print("random sampling of five values:")
+print(bikes.sample(5))
+print()
+
+#collecting and removing null values
+nullvals = pd.isnull(bikes)
+pd.set_option('display.max_columns',100)
+print(nullvals.describe())
+
+
+
+
