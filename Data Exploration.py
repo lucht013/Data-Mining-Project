4f37c768fed5527db535e0a602f5576a1fb31b2c
Robert-Beane <beane031@morris.umn.edu>
2020-11-14 20:44:52 -0600

    Add a scatter matrix along with some histograms

--- Data Exploration.py
+++ Data Exploration.py
@@ -26,14 +26,16 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
 bikes = pd.read_csv("london_merged.csv")
 df = bikes.copy()
-#Looking through the data
+# Looking through the data
 
 print(bikes.columns)
 
@@ -53,21 +55,23 @@ print("random sampling of five values:")
 print(bikes.sample(5))
 print()
 
-#collecting and removing null values
+# Collecting and removing null values
 nullvals = pd.isnull(bikes)
-pd.set_option('display.max_columns',100)
+pd.set_option('display.max_columns', 100)
 print(nullvals.describe())
+
 
 # Changing celsius to fahrenheit
 def to_fahrenheit(x):
-    return x * (9/5) + 32
+    return x * (9 / 5) + 32
 
-bikes['t1'] = bikes['t1'].apply(to_fahrenheit) #changes temps to fahrenheit for the real temp
-bikes['t2'] = bikes['t2'].apply(to_fahrenheit) #changes "feels like" temp to fahrenheit
+
+bikes['t1'] = bikes['t1'].apply(to_fahrenheit)  # changes temps to fahrenheit for the real temp
+bikes['t2'] = bikes['t2'].apply(to_fahrenheit)  # changes "feels like" temp to fahrenheit
 print(bikes.t1)
 print(bikes.t2)
 
-bikes[['Date','Time']] = bikes.timestamp.str.split(expand=True)
+bikes[['Date', 'Time']] = bikes.timestamp.str.split(expand=True)
 bikes = bikes.drop('timestamp', axis=1)
 
 """
@@ -80,17 +84,17 @@ corr = bikes.corr()
 sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, fmt=".1f", cmap='RdBu', annot=True)
 plt.show()
 
-#training data/building Linear regression
-excluded = ('Date','Time','cnt')
-#predictors = [s for s in bikes.columns if s not in excluded]
+# training data/building Linear regression
+excluded = ('Date', 'Time', 'cnt')
+# predictors = [s for s in bikes.columns if s not in excluded]
 predictors = ['hum']
 outcome = 'cnt'
 X = bikes[predictors]
 y = bikes[outcome]
-train_X, valid_X, train_y, valid_y = train_test_split(X, y, test_size = 0.4, random_state = 1)
+train_X, valid_X, train_y, valid_y = train_test_split(X, y, test_size=0.4, random_state=1)
 
 model = LinearRegression()
-model.fit(train_X,train_y)
+model.fit(train_X, train_y)
 
 train_pred = model.predict(train_X)
 train_results = pd.DataFrame({'cnt': train_y, 'predicted': train_pred, 'residual': train_y - train_pred})
@@ -98,4 +102,31 @@ print("And here are the results!")
 print("")
 print(train_results.head())
 
+# scatter plots
 
+df.plot.scatter(x='cnt', y='t1', legend=False)
+plt.show()
+
+df = bikes[['cnt', 't1', 't2', 'wind_speed', 'hum', 'weather_code']]
+axes = scatter_matrix(df, alpha=0.5, figsize=(6, 6), diagonal='kde')
+corr = df.corr().values
+for i, j in zip(*plt.np.triu_indices_from(axes, k=1)):
+    axes[i, j].annotate("%.3f" % corr[i, j], (0.8, 0.8),
+                        xycoords='axes fraction', ha='center', va='center')
+plt.show()
+
+# histogram
+ax = bikes.hum.hist()
+ax.set_xlabel('Humidity')
+ax.set_ylabel('Count')
+plt.show()
+
+ax = bikes.t1.hist()
+ax.set_xlabel('Temperature (in F)')
+ax.set_ylabel('Count')
+plt.show()
+
+ax = bikes.wind_speed.hist()
+ax.set_xlabel('Wind Speed')
+ax.set_ylabel('Count')
+plt.show()
